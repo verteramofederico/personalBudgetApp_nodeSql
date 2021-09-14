4a1aeae2517df9dfe2f24ae94e09b42b0cb25051
verteramo.federico <verteramo.federico@gmail.com>
2021-09-14 11:02:20 -0300

    express server running

---
+++ src/app.js
@@ -0,0 +1,23 @@
+let express = require('express');
+let path = require('path');
+let app = express();
+
+
+app.set('view engine', 'ejs');
+
+// view engine setup
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'ejs');
+
+app.get('/', (req, res) => {
+    res.render('home')
+    }
+    )
+    
+
+app.use(express.static('public'))
+
+app.listen(3000, () =>
+console.log('Servidor corriendo en http://localhost:3000/')
+);
+
